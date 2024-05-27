40c444ad771e2179b760b8c0809754e7ad683c00
pradishtha <164838915+pradishtha@users.noreply.github.com>
2024-05-27 15:24:19 +0530

    Update LAB5.py

--- LAB5.py
+++ LAB5.py
@@ -1,21 +1,21 @@
-# import necessary libarities
 import pandas as pd
-from sklearn import tree
 from sklearn.preprocessing import LabelEncoder
 from sklearn.naive_bayes import GaussianNB
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
 
-# load data from CSV
+# Load data from CSV
 data = pd.read_csv('tennisdata.csv')
-print("THe first 5 values of data is :\n",data.head())
+print("The first 5 values of data are:\n", data.head())
 
-# obtain Train data and Train output
-X = data.iloc[:,:-1]
-print("\nThe First 5 values of train data is\n",X.head())
+# Obtain Train data and Train output
+X = data.iloc[:, :-1]
+print("\nThe First 5 values of train data are:\n", X.head())
 
-y = data.iloc[:,-1]
-print("\nThe first 5 values of Train output is\n",y.head())
+y = data.iloc[:, -1]
+print("\nThe first 5 values of Train output are:\n", y.head())
 
-# Convert then in numbers 
+# Convert them to numbers
 le_outlook = LabelEncoder()
 X.Outlook = le_outlook.fit_transform(X.Outlook)
 
@@ -28,17 +28,15 @@ X.Humidity = le_Humidity.fit_transform(X.Humidity)
 le_Windy = LabelEncoder()
 X.Windy = le_Windy.fit_transform(X.Windy)
 
-print("\nNow the Train data is :\n",X.head())
+print("\nNow the Train data is:\n", X.head())
 
 le_PlayTennis = LabelEncoder()
 y = le_PlayTennis.fit_transform(y)
-print("\nNow the Train output is\n",y)
+print("\nNow the Train output is:\n", y)
 
-from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=0.20)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20)
 
 classifier = GaussianNB()
-classifier.fit(X_train,y_train)
+classifier.fit(X_train, y_train)
 
-from sklearn.metrics import accuracy_score
-print("Accuracy is:",accuracy_score(classifier.predict(X_test),y_test))
+print("Accuracy is:", accuracy_score(classifier.predict(X_test), y_test))
